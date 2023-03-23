55fdafebb211e67fcd245a6273d21b56d37922e0
GABYZUL <104597512+GABYZUL@users.noreply.github.com>
2023-03-23 17:36:56 -0600

    Update principal-routing.module.ts

--- src/app/modulos/principal/principal-routing.module.ts
+++ src/app/modulos/principal/principal-routing.module.ts
@@ -20,7 +20,9 @@ const rutas: Routes = [
     {path:'equipos', component: EquiposComponent, },
     {path:'administrador', component: EquiposComponent},
     {path:'valorequipos', component: ValorequiposComponent},
-    {path:'checklist', component: ChecklistComponent}
+    {path:'checklist', component: ChecklistComponent},
+    {path:'disponibilidaddiaria', component: DisponibilidaddiariaComponent},
+
     //{path:'equipos', component: EquiposComponent},
     //{path:'valorequipos', component: ValorequiposComponent},
     //{path:'checklist', component:ChecklistComponent},
