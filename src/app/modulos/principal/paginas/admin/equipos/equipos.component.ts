e2be7187f30dcfcf0a6a36635ac71a06768bddd0
GABYZUL <104597512+GABYZUL@users.noreply.github.com>
2023-03-28 19:23:17 -0600

    paginado

--- src/app/modulos/principal/paginas/admin/equipos/equipos.component.ts
+++ src/app/modulos/principal/paginas/admin/equipos/equipos.component.ts
@@ -106,6 +106,13 @@ export class EquiposComponent implements OnInit {
       }
     })
   }
+
+
+  nAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
